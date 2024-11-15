d6b6a02a6a78f37c06e63582ec96d788ee32872f
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-11-14 20:16:14 -0600

    Add files via upload

---
+++ prac html-4/html.py
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<nav>
+  <a href="/Bootstrap Versions/">Bootstrap Versions</a> |
+  <a href="/C++/">C++</a> |
+  <a href="/js/">JavaScript</a> |
+  <a href="/jquery/">jQuery</a>
+</nav>
+
+</body>
+</html>
