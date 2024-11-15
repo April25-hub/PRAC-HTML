ba8b4c8d1ed8454f326ba6011f5eebfea0f2e3bf
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-11-14 20:10:59 -0600

    Add files via upload

---
+++ pract hrml 3/html.py
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<footer>
+  <p>Autor: Juan Pérez</p>
+  <p><a href="https://www.mexicodesconocido.com.mx/wp-content/uploads/2023/08/6119-GAL-dsc_0785-scaled.jpg">https://www.mexicodesconocido.com.mx/wp-content/uploads/2023/08/6119-GAL-dsc_0785-scaled.jpg</a></p>
+</footer>
+
+</body>
+</html>
