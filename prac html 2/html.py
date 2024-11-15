cefba87b88d398e0c64439ad198d7bcc3c31ca7f
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-11-14 19:59:51 -0600

    Add files via upload

---
+++ prac html 2/html.py
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.all-dances {
+  margin: 0;
+  padding: 5px;
+  background-color: lightgray;
+}
+
+.all-dances > h1, .dance {
+  margin: 10px;
+  padding: 5px;
+}
+
+.dance {
+  background: white;
+}
+
+.dance > h2, p {
+  margin: 4px;
+  font-size: 90%;
+}
+</style>
+</head>
+<body>
+
+<article class="all-dances">
+  <h1>Danzas Folkloricas Mas Populares</h1>
+  <article class="dance">
+    <h2>Jarabe Tapatio</h2>
+    <p>El Jarabe Tapatio es una danza tradicional mexicana originaria de Jalisco. Es conocida por su musica alegre y por los coloridos trajes, incluyendo el famoso sombrero de charro.</p>
+  </article>
+  <article class="dance">
+    <h2>Cumbia</h2>
+    <p>La cumbia es un genero musical y danza folklorica originaria de Colombia, pero que se ha popularizado en varios paises de America Latina. Su caracteristico ritmo y movimientos sensuales la han hecho muy popular en todo el continente.</p>
+  </article>
+  <article class="dance">
+    <h2>Valsecito</h2>
+    <p>El Valsecito es una danza de origen europeo, especialmente popular en paises como Argentina y Bolivia. Se caracteriza por su compas de tres tiempos y los giros suaves de los bailarines.</p>
+  </article>
+</article>
+
+</body>
+</html>
