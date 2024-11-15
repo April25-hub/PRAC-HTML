23faf1f6e019e177ca36f19d336d9a44373157e0
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-11-14 20:20:30 -0600

    Add files via upload

---
+++ prac html 5/html.py
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>La danza folklorica es un arte muy bello</p>
+
+<aside>
+  <h4>LA DANZA FOLKLORICA INFORMACION</h4>
+  <p>La danza folklórica, danza folclórica o danza típica es un tipo de baile social, a menudo practicado en grupos, que forma parte del acervo o la tradición de una sociedad o una cultura.</p>
+</aside>
+
+</body>
+</html>
